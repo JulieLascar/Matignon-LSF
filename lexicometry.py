a201b6b78c5a63bed95e794d029d9430e91fd83e
julie921 <halbout.julie@gmail.com>
2024-01-18 12:38:34 +0100

    modification du script lexicometry.py pour ajouter en argument un dossier, et création des figures matplotlib (histogramme)

--- lexicometry.py
+++ lexicometry.py
@@ -10,15 +10,17 @@ import json
 import csv
 from tqdm import tqdm
 from typing import List
-
+import matplotlib.pyplot as plt
+import argparse
 """
 Le script donne en sortie des informations sur le corpus et trois fichiers :
 - information_corpus.txt : fichier texte avec des informations sur le nombre de token et la taille du vocabulaire
 - data_subtitles.json : dictionnaire avec les VERB, ADJ, NOUN et PROPN trier par fréquence
 - data_subtitles.csv : dictionnaire avec les VERB, ADJ, NOUN et PROPN trier par fréquence
+Pour l'utiliser : python lexicometry.py --subtitles_folder PATH/TO/FOLDER
 """
 
-def lister_fichiers(chemin:str)->list(str):
+def lister_fichiers(chemin:str)->list:
     """
     La fonction prend en entrée un no mde dossier et renvoie la liste des fichiers qu'il contient.
 
@@ -67,7 +69,11 @@ def json_to_text(file:str)->str:
 
 
 if __name__=="__main__":
-    dossier_cible = "subtitles"
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--subtitles_folder', help='dossier de sous titre à traiter')
+    args = parser.parse_args()
+    dossier_cible = args.subtitles_folder 
+
     fichiers_dans_dossier = lister_fichiers(dossier_cible)
 
     whole_text = ""
@@ -88,11 +94,12 @@ if __name__=="__main__":
     # on récupère le vocabulaire (lemma)
     voc = [token.lemma_ for token in doc]
 
-    # et on le compte 
+    # et on compte le nombre d'occurrence par lemme
     c = Counter()
     for element in voc:
         c[element] +=1
 
+    # on récupère la taille du vocabulaire (nombre de lemme différent)
     taille_voc = len(c)
 
     tt = f"INFORMATIONS CORPUS : \n\n nombre de tokens = {nb_token} \n taille du vocabulaire = {taille_voc}"
@@ -136,3 +143,68 @@ if __name__=="__main__":
         for key,value in dict_lex.items():
             lemma,pos = key
             writer.writerow({'Lemme': lemma, 'POS': pos, 'frequence': value})
+
+    # on créer des histogrammes : 
+    # pour les NOUN, ADJ, PROPN et VERB du corpus mélangé (trié par fréquece d'occurrence)
+    new_dict={}
+    for i, (key, value) in enumerate(dict_lex.items()):
+        lemma, pos = key
+        freq = value
+        new_dict[lemma]=freq
+        if i == 21:
+            break
+    
+    # et un dictionnaire pour chaque POS enregistré (NOUN, VERB, ADJ, PROPN)
+    new_dict_verb = {}
+    new_dict_noun = {}
+    new_dict_adj = {}
+    new_dict_propn = {}
+    new_dict = {}
+    # Limite pour chaque dictionnaire
+    limite = 20
+
+    for key, value in dict_lex.items():
+        lemma, pos = key
+        freq = value
+
+        if pos == 'VERB' and len(new_dict_verb) < limite:
+            new_dict_verb[lemma] = freq
+
+        elif pos == 'NOUN' and len(new_dict_noun) < limite:
+            new_dict_noun[lemma] = freq
+
+        elif pos == 'ADJ' and len(new_dict_adj) < limite:
+            new_dict_adj[lemma] = freq
+
+        elif pos == 'PROPN' and len(new_dict_propn) < limite:
+            new_dict_propn[lemma] = freq
+        
+        # Vérifier si toutes les limites sont atteintes
+        if len(new_dict_verb) == limite and len(new_dict_noun) == limite and len(new_dict_adj) == limite and len(new_dict_propn) == limite and len(new_dict)==limite:
+            break
+
+    # liste des sous-dictionnaires de 20 items : 
+    list_dict = [new_dict,new_dict_adj,new_dict_noun,new_dict_propn,new_dict_verb]
+
+    # création des figures - enregistrement 
+    for title,dictionnaire in zip(['All','ADJ','NOUN','PROPN','VERB'],list_dict):
+        data = dictionnaire
+        names = list(data.keys())
+        values = list(data.values())
+
+        # Créer une seule figure avec un axe
+        fig, ax = plt.subplots(figsize=(9, 3))
+
+        # Tracer l'histogramme
+        ax.bar(names, values)
+
+        # Ajouter un titre à la figure
+        fig.suptitle(f'Histogramme des fréquences de {title}')
+
+        # Faire pivoter les étiquettes de l'axe des abscisses
+        ax.set_xticklabels(names, rotation=45, ha='right')
+
+        #Enregistrer la figure
+        plt.savefig(f'figure_frequence_{title}.png',bbox_inches='tight')
+
+    
