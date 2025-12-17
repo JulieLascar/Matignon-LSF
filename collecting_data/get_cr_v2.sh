14ebf0f2060864e24ee1c0dbd62b49c7c10412fa
Julie921 <98755655+Julie921@users.noreply.github.com>
2025-12-17 09:42:10 +0100

    Add files via upload
    scrapping Matignon-LSF, new script

---
+++ collecting_data/get_cr_v2.sh
@@ -0,0 +1,3 @@
+yt-dlp -f bestvideo --extract-audio --audio-format mp3 --merge-output-format mp4 --wirte-subs --sub-langs fr-FR --yes-playlist https://www.youtube.com/playlist?list=PLj6LC69kREj-f\_v8NBGb4CgatbwO7xi\_2&jct=YC8XD-GfupTjwab6lTbBmQyt-dlp -f bestvideo --extract-audio --audio-format mp3 --merge-output-format mp4 --wirte-subs --sub-langs fr-FR --yes-playlist https://www.youtube.com/playlist?list=PLj6LC69kREj-f\_v8NBGb4CgatbwO7xi\_2&jct=YC8XD-GfupTjwab6lTbBmQ
+
+
